26892a938b882ff04cb6598989955488d09f352a
Blaueseichoern <maki2012@gmx.de>
2012-10-11 20:12:16 +0200

    further setup

--- src/com/connection/Communicator.java
+++ src/com/connection/Communicator.java
@@ -1,11 +1,18 @@
 package com.connection;
 
+import com.screen.ComWindow;
+
 public class Communicator 
 {
 
 	public void handleInput(Object obj) 
 	{
 		// TODO 	
+	}
+	
+	public static void main(String[] args)
+	{
+		ComWindow com = new ComWindow(new Communicator());
 	}
 	
 }
--- src/com/screen/ComWindow.java
+++ src/com/screen/ComWindow.java
@@ -1,5 +1,7 @@
 package com.screen;
 
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -7,49 +9,69 @@ import java.awt.Insets;
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 import com.connection.Communicator;
 import com.connection.Participant;
 
-public class ComWindow extends JFrame
+public class ComWindow
 {
 	private static final long serialVersionUID = 5333176854570891107L;
 	
 	private Communicator communicator;
 	
+	JFrame main;
 	JList<Participant> participants;
+	JPanel generalChat;
+	JTabbedPane chats;
 	MessageField messages;
 	TextInput input;
 	
 	public ComWindow(Communicator com)
 	{
-		super("Classroom Communicator");
+		main = new JFrame("Classroom Communicator");
 		
 		communicator = com;
 		
+		
 		GridBagLayout gbl = new GridBagLayout();
-		this.setLayout(gbl);
-		GridBagConstraints gbc = new GridBagConstraints();
+		main.setLayout(gbl);
 		
-		gbc.insets = new Insets(4, 4, 4, 4);
-		
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.gridheight = 2;
-		
+		//LISTE
 		DefaultListModel<Participant> model = new DefaultListModel<Participant>();
 		participants = new JList<Participant>(model);
 		
-		gbl.setConstraints(participants, gbc);
-		add(participants);
+		//CHATS
+		chats = new JTabbedPane();
+		generalChat = new JPanel();
+		chats.addTab("general Chat", generalChat);
 		
-		gbc.gridx=1;
-		gbc.gridy=0;
-		gbc.gridheight = 2;
-		gbc.gridwidth = 2;
+		Container c = main.getContentPane();
 		
+		addComponent(c, gbl, participants, 0, 0, 1, 2, 0, 0);
+		addComponent(c, gbl, chats, 1, 0, 2, 2, 0, 0);
 		
-		this.setSize(1024, 720);
+		main.setSize(1024, 720);
+		
+		main.setVisible(true);
 	}
+	
+    static void addComponent( Container cont,
+            GridBagLayout gbl,
+            Component c,
+            int x, int y,
+            int width, int height,
+            double weightx, double weighty )
+    {
+        
+       GridBagConstraints gbc = new GridBagConstraints();
+       gbc.fill = GridBagConstraints.BOTH;
+       gbc.gridx = x; gbc.gridy = y;
+       gbc.gridwidth = width; gbc.gridheight = height;
+       gbc.weightx = weightx; gbc.weighty = weighty;
+       gbl.setConstraints( c, gbc );
+       cont.add( c );
+   }
 	
 }
