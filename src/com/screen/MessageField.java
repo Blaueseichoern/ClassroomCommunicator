2c535132423742cdcc464a3dbbd30e8cff2ac3d1
Blaueseichoern <maki2012@gmx.de>
2012-10-11 17:00:00 +0200

    First setup

---
+++ src/com/screen/MessageField.java
@@ -0,0 +1,13 @@
+package com.screen;
+
+import javax.swing.JTextArea;
+/**
+ * The output of the received Messages
+ * @author Markus
+ *
+ */
+public class MessageField extends JTextArea
+{
+	private static final long serialVersionUID = -5899704500148560887L;
+
+}
---
+++ src/com/screen/TextInput.java
@@ -0,0 +1,13 @@
+package com.screen;
+
+import javax.swing.JTextArea;
+/**
+ * The inputfield of the text to send
+ * @author Markus
+ *
+ */
+public class TextInput extends JTextArea
+{
+	private static final long serialVersionUID = 3691437442214520942L;
+
+}
