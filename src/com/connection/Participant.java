d2d9047b7965438de0266dbf12689a335efb6322
Blaueseichoern <maki2012@gmx.de>
2012-10-11 17:47:35 +0200

    First sutup

---
+++ src/com/connection/Participant.java
@@ -0,0 +1,10 @@
+package com.connection;
+
+import java.net.Socket;
+
+public class Participant 
+{
+	String name;
+	/** maybe useless*/
+	Socket socket;
+}
---
+++ src/com/main/Communicator.java
@@ -0,0 +1,14 @@
+package com.main;
+
+/**
+ * The main class
+ * @author Markus
+ *
+ */
+public class Communicator 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
